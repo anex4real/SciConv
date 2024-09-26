1eca86bed79dba6d27b606b02f4ea828bd0ad993
lazaro <lazaroosta@hotmail.com>
2024-09-26 11:50:49 +0100

    update

--- backendChat/settings.py
+++ backendChat/settings.py
@@ -109,10 +109,14 @@ def startDockerClient():
             print("Using Docker socket from Docker Desktop: /home/lazaro/.docker/desktop/docker.sock")
             client = docker.DockerClient(base_url='unix:///home/lazaro/.docker/desktop/docker.sock')
         # Check if the default Docker socket exists
-        elif os.path.exists('/var/run/docker.sock'):
-            print("Using default Docker socket: /var/run/docker.sock")
-            client = docker.DockerClient(base_url='unix:///var/run/docker.sock')
+        elif os.path.exists('/home/ubuntu/my_docker.sock'):
+            print("Using default Docker socket: /home/ubuntu/my_docker.sock")
+            client = docker.DockerClient(base_url='unix:///home/ubuntu/my_docker.sock')
         # Check for the Docker socket used by Docker Desktop
+        # elif os.path.exists('/var/run/docker.sock'):
+        #     print("Using default Docker socket: /var/run/docker.sock")
+        #     client = docker.DockerClient(base_url='unix:///var/run/docker.sock')
+        # # Check for the Docker socket used by Docker Desktop
         else:
             # If no specific socket is found, fall back to using environment variables
             print("No specific Docker socket found, attempting to use docker.from_env()")
