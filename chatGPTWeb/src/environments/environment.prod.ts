ec614a1a17247d5c756cca1d32923d5e7f869ffb
lazaro <lazaroosta@hotmail.com>
2024-10-04 00:52:50 +0100

    update

--- chatGPTWeb/src/environments/environment.prod.ts
+++ chatGPTWeb/src/environments/environment.prod.ts
@@ -1,9 +1,10 @@
 export const environment = {
   production: true,
   //baseUrl: 'http://localhost:8080',
-  baseUrl: 'http://backend:8080'
+  //baseUrl: 'http://backend:8080'
+  baseUrl: 'http://10.61.12.140:8080'
   //baseUrl: 'http://127.0.0.1:8080'
-  //baseUrl: 'http://10.61.12.140:8080'
+
   //baseURL: 'http:/194.117.30.76'
   // baseUrl: 'http://sciconv.inesctec.pt:8080',
 };
