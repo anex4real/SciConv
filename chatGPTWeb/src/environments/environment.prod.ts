b6ec969c264ef029b484ccc00edb459a6bcf080b
lazaro <lazaroosta@hotmail.com>
2024-10-03 17:59:31 +0100

    goback functional

--- chatGPTWeb/src/environments/environment.prod.ts
+++ chatGPTWeb/src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
+  //baseUrl: 'http://localhost:8080,
   //baseUrl: 'http://backend:8080'
   //baseUrl: 'http://127.0.0.1:8080'
   //baseUrl: 'http://10.61.12.140:8080'
