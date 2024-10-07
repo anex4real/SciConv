c1f910a8ba6c61b8f0cfb43463364a4c56b9fa7c
lazaro <lazaroosta@hotmail.com>
2024-10-07 23:27:44 +0100

    update

--- chatGPTWeb/src/environments/environment.prod.ts
+++ chatGPTWeb/src/environments/environment.prod.ts
@@ -5,10 +5,10 @@ export const environment = {
   //baseUrl: 'http://10.61.12.96:8080'
   //baseUrl: 'http://127.0.0.1:8080'
 
-  baseUrl: 'http://sciconv.inesctec.pt:8080',
+  //baseUrl: 'http://sciconv.inesctec.pt:8080',
   //funcionou com as portas abertas
 
   //baseUrl: 'http://194.117.30.76:8080', //funcionou
 
-  //baseUrl: ' http://172.18.0.3:8080' nao deu
+  baseUrl: ' http://172.20.0.2:8080' //nao deu
 };
