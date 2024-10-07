76c723e62a065a5b27a69245ead6c214a9d4c46e
lazaro <lazaroosta@hotmail.com>
2024-10-07 22:36:16 +0100

    update

--- backendChat/main.py
+++ backendChat/main.py
@@ -15,8 +15,8 @@ QUESTIONNAIRES_LOCATION = 'questionnaires'
 
 app = Flask(__name__)
 #cors = CORS(app, resources={r"/*": {"origins": "*"}})
-cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
-#CORS(app)  # This will allow CORS for all routes by default
+#cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+CORS(app)  # This will allow CORS for all routes by default
 
 
 
