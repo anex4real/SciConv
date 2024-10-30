a7ef2300b456ac6d5d6d94946828f7ebe41a8806
lazaro <lazaroosta@hotmail.com>
2024-10-30 16:25:11 +0000

    update

--- chatGPTWeb/src/environments/environment.prod.ts
+++ chatGPTWeb/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  //baseUrl: 'http://localhost:8080',
+  baseUrl: 'http://localhost:8080',
   //baseUrl: 'http://backend:8080'
   //baseUrl: 'http://10.61.12.96:8080'
   //baseUrl: 'http://127.0.0.1:8080'
@@ -11,6 +11,6 @@ export const environment = {
   //baseUrl: 'http://194.117.30.76:8080', //funcionou
 
   //baseUrl: ' http://172.18.0.3:8080' nao deu
-  baseUrl: '',
+  //baseUrl: '',
 
 };
